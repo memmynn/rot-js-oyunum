cc01c7c3fa28d6d110e4aca6ce4f4eb05c760516
memmynn <mehmetuyarwww@hotmail.com>
2021-08-27 17:49:13 +0300

    event listeners added

--- game.js
+++ game.js
@@ -1,3 +1,32 @@
+//array'den random item alma
+function getRandom(array){
+    return Math.floor(Math.random()*array.length)
+};
+//input callback
+function lightPasses(x, y){
+    let key = x+","+y;
+    if(key in map) {return (map[key] === 0);}
+    return false;
+};  
+//haritada gösterme
+function show(param) {
+    display.draw(param.x, param.y, param.avatar)
+};
+
+//harita draw
+function drawAll () {
+    show(portal);
+    fovCompute(player);
+}
+//geçilebilir olma testi
+function isPassible(x, y){
+    let key = x+","+y;
+    if(key in map && map[key] === 0) {return true};
+    return false;
+};
+
+
+//========================================HARİTA==============================================================
 let displayOptions = {
     width: 55,
     height: 20,
@@ -12,6 +41,7 @@ let displayOptions = {
 let display = new ROT.Display(displayOptions);
 
 document.body.append(display.getContainer());
+
 let map = {};
 let freeCells = [];
 
@@ -20,37 +50,46 @@ new ROT.Map.DividedMaze(displayOptions.width, displayOptions.height).create(func
     if(type === 0) {freeCells.push({"x":x, "y":y})}
     display.DEBUG(x, y, type);
 });
+//======================================================================================================
 
-//input callback
-function lightPasses(x, y){
-    let key = x+","+y;
-    if(key in map) {return (map[key] === 0);}
-    return false;
-};  
-
-function show(param) {
-    display.draw(param.x, param.y, param.avatar)
+//haritadaki karakter sınıfı
+let character = function (avatar) {
+    this.x;
+    this.y;
+    this.avatar = avatar;
 };
 
-let player = {
-    x: null,
-    y: null,
-    avatar: "@",
-};
-
-Object.assign(player, freeCells[30]);
-
-function isPassible(x, y){
-    let key = x+","+y;
-    if(key in map && map[key] === 1) {return true};
-    return false;
-}
+let player = new character("@");
+let portal = new character("#");
+//player ve portal'ı rastgele noktaya atma
+Object.assign(player, freeCells[getRandom(freeCells)]);
+Object.assign(portal, freeCells[getRandom(freeCells)]);
+Object.assign
+//event listener ekleme
+document.addEventListener('keydown', (event) => {
+    let code = event.code;
+    if (code === "ArrowLeft"){
+        if (isPassible(player.x - 1, player.y)) {player.x -=1};
+        console.log("arrowleft");
+    };if (code === "ArrowRight"){
+        if (isPassible(player.x + 1, player.y)) {player.x +=1};
+    };if (code === "ArrowUp"){
+        if (isPassible(player.x, player.y - 1)) {player.y -=1};
+    };if (code === "ArrowDown"){
+        if (isPassible(player.x, player.y + 1)) {player.y +=1};
+    };
+    drawAll();
+});
 
 let fov = new ROT.FOV.PreciseShadowcasting(lightPasses);
+
 //output callback
-fov.compute(player.x, player.y, 15, function(x, y, r, visibility) {
-    let ch = (r ? "" : player.avatar);
-    let color = (map[x+","+y] ? "#aa0":"#660" );
-    display.draw(x, y, ch, "#fff", color)
-} );
+function fovCompute(char) {
+        fov.compute(char.x, char.y, 15, function(x, y, r, visibility) {
+        let ch = (r ? "" : char.avatar);
+        let color = (map[x+","+y] ? "#aa0":"#660" );
+        display.draw(x, y, ch, "#fff", color);
+    } );
+};
+
 
